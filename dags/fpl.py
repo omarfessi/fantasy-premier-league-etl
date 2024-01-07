44b3adb5800bd235494d149d94657facd2b26be0
Omar <omarfessy@gmail.com>
2024-01-07 02:56:47 +0100

    get rid of postgresop that create teams table and use custom op instead

--- dags/fpl.py
+++ dags/fpl.py
@@ -1,29 +1,28 @@
 import datetime
 
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.dummy_operator import DummyOperator
-
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from operators import CreateTablesOperator
 
 DAG_ID = "fpl_elt"
+POSTGRES_CONN_ID = "airflow_pg_cnx"
 
 with DAG(
     dag_id=DAG_ID,
     start_date=datetime.datetime(2022, 1, 5),
     schedule_interval="0 0 1 * *",
     catchup=False,
-    template_searchpath=["templates/CREATIONS", "rendered_templates"],
+    template_searchpath=["rendered_templates"],
 ) as dag:
     start_operator = DummyOperator(task_id="begin_execution")
-    create_teams_table = PostgresOperator(
-        task_id="create_teams_table",
-        sql="teams.sql",
-        postgres_conn_id="airflow_pg_cnx",
+    create_all_tables = CreateTablesOperator(
+        task_id="create_all_tables", postgres_conn_id=POSTGRES_CONN_ID, dag=dag
     )
     populate_teams_table = PostgresOperator(
         task_id="populate_teams_table",
         sql="teams/renderered_sql_20240106_1742.sql",
-        postgres_conn_id="airflow_pg_cnx",
+        postgres_conn_id=POSTGRES_CONN_ID,
     )
 
-    start_operator >> create_teams_table >> populate_teams_table
+    start_operator >> create_all_tables >> populate_teams_table
--- plugins/create_tables_sql.py
+++ plugins/create_tables_sql.py
@@ -1,6 +1,10 @@
+teams_table = """
 CREATE table IF NOT exists public.teams(
    id INTEGER,
    name VARCHAR NOT NULL ,
    short_name VARCHAR NOT NULL ,
    PRIMARY KEY(id)
-);
+);"""
+
+
+create_all_tables_statement = [teams_table]
