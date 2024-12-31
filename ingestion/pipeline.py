234700c467e344bb771eca1026918890b957b94c
Omar <omarfessy@gmail.com>
2024-12-31 18:43:34 +0100

    add a main pipeline for ingestion raw data

---
+++ ingestion/pipeline.py
@@ -0,0 +1,42 @@
+import logging
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+from config import FIXTURES_URL, STATIC_URL
+from models import Event, Fixture, ModelUnion, Player, Team
+from utilities import call_api, extract_and_validate_entities
+
+logging.basicConfig(level=logging.INFO)
+
+
+def process_entity(entity_name: str, url: str, model: ModelUnion, filename: str):
+    """
+    Generic function to process entities: fetch, validate, and save to file.
+
+    Args:
+        entity_name (str): Name of the entity for logging.
+        url (str): API endpoint to fetch data from.
+        model (BaseModel): Pydantic model for validation.
+        schema_method (callable): Method to generate PyArrow schema.
+        filename (str): File name for saving data.
+    """
+    logging.info(f"Processing {entity_name}...")
+    data = call_api(url).json()
+    if isinstance(data, dict):
+        data = data.get(entity_name, [])
+    validated_data = extract_and_validate_entities(data, model)
+    serialized_data = [data.model_dump() for data in validated_data]
+    table = pa.Table.from_pylist(serialized_data, schema=model.pyarrow_schema())
+    pq.write_table(table, filename)
+    logging.info(f"Completed processing {entity_name}.")
+
+
+if __name__ == "__main__":
+
+    for entity in [
+        ("fixtures", FIXTURES_URL, Fixture, "fixtures.parquet"),
+        ("players", STATIC_URL, Player, "players.parquet"),
+        ("events", STATIC_URL, Event, "events.parquet"),
+        ("teams", STATIC_URL, Team, "teams.parquet"),
+    ]:
+        process_entity(*entity)
