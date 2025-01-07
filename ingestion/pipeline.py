3599a5c5558737dc668f6ff68f414d8aea11a969
Omar <omarfessy@gmail.com>
2025-01-07 16:10:44 +0100

    use buffer to load data into duckdb

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -1,18 +1,17 @@
 import logging
+from datetime import date
 
 import pyarrow as pa
 import pyarrow.parquet as pq
 
 from .config import FIXTURES_URL, STATIC_URL
 from .models import Event, Fixture, ModelUnion, Player, Team
-from .utilities import call_api, extract_and_validate_entities
+from .utilities import TableLoadingBuffer, call_api, extract_and_validate_entities
 
 logging.basicConfig(level=logging.INFO)
 
 
-def process_entity(
-    entity_name: str, url: str, model: ModelUnion, filename: str
-) -> None:
+def process_entity(entity_name: str, url: str, model: ModelUnion) -> pa.Table | None:
     """
     Generic function to process entities: fetch, validate, and save to file.
 
@@ -35,19 +34,25 @@ def process_entity(
     if validated_data:
         serialized_data = [data.model_dump() for data in validated_data]
         table = pa.Table.from_pylist(serialized_data, schema=model.pyarrow_schema())
-        pq.write_table(table, filename)
         logging.info(f"Completed processing {entity_name}.")
+        return table
     else:
-        logging.warning(
-            f"No data processed neither inserted into destination for {entity_name}."
-        )
+        logging.warning(f"No data processed for {entity_name}.")
 
 
 if __name__ == "__main__":
+    today = date.today().strftime("%Y_%m_%d")
+    buffer = TableLoadingBuffer()
     for entity in [
-        ("fixtures", FIXTURES_URL, Fixture, "fixtures.parquet"),
-        ("elements", STATIC_URL, Player, "players.parquet"),
-        ("events", STATIC_URL, Event, "events.parquet"),
-        ("teams", STATIC_URL, Team, "teams.parquet"),
+        ("fixtures", FIXTURES_URL, Fixture, f"fixtures_{today}.parquet"),
+        ("elements", STATIC_URL, Player, f"players_{today}.parquet"),
+        ("events", STATIC_URL, Event, f"events_{today}.parquet"),
+        ("teams", STATIC_URL, Team, f"teams_{today}.parquet"),
     ]:
-        process_entity(*entity)
+        table = process_entity(*entity[0:3])
+        if table:
+            logging.info(f"Writing {entity[0]} to file...")
+            pq.write_table(table, entity[-1])
+            logging.info(f"Loading {entity[0]} to database...")
+            buffer.insert_table(entity[0], table, entity[2].duckdb_schema())
+    buffer.close_connection()
