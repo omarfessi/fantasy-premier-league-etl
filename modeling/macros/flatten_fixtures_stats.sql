18963e39ad580fbbd23e84a037322c75ca269088
Omar <omarfessy@gmail.com>
2025-01-11 20:59:59 +0100

    get rid of ephemeral model and refactor macro to only depend on it

--- modeling/macros/flatten_fixtures_stats.sql
+++ modeling/macros/flatten_fixtures_stats.sql
@@ -1,17 +1,38 @@
 {% macro flatten_stats_from_raw_fixtures(stat_type) %}
-(
-    SELECT
+    WITH flat_raw_fx_stats AS (
+        SELECT
+            code,
+            event AS gameweek,
+            kickoff_time,
+            CASE 
+                WHEN flatten_stats.unnest.h IS NOT NULL THEN UNNEST(flatten_stats.unnest.h) 
+            END AS h_player_stats,
+            CASE 
+                WHEN flatten_stats.unnest.a IS NOT NULL THEN UNNEST(flatten_stats.unnest.a) 
+            END AS a_player_stats
+        FROM {{source('landing', 'raw_fixtures' )}} t
+        CROSS JOIN UNNEST(t.stats) AS flatten_stats
+        WHERE flatten_stats.unnest.identifier = '{{ stat_type }}'
+    )
+
+    SELECT 
         code,
-        event AS gameweek,
-        kickoff_time,
-        CASE 
-            WHEN flatten_stats.unnest.h IS NOT NULL THEN UNNEST(flatten_stats.unnest.h) 
-        END AS h_player_stats,
-        CASE 
-            WHEN flatten_stats.unnest.a IS NOT NULL THEN UNNEST(flatten_stats.unnest.a) 
-        END AS a_player_stats
-    FROM {{source('landing', 'raw_fixtures' )}} t
-    CROSS JOIN UNNEST(t.stats) AS flatten_stats
-    WHERE flatten_stats.unnest.identifier = '{{ stat_type }}'
-)
+        gameweek,
+        kickoff_time, 
+        h_player_stats.value as stat_value, 
+        h_player_stats.element as player_id,
+        'home' AS side
+    FROM flat_raw_fx_stats 
+    WHERE h_player_stats.value not null
+    UNION ALL
+    SELECT 
+        code,
+        gameweek,
+        kickoff_time, 
+        a_player_stats.value as stat_value, 
+        a_player_stats.element as player_id,
+        'away' AS side
+    FROM flat_raw_fx_stats
+    WHERE a_player_stats.value not null
+
 {% endmacro %}
--- modeling/models/facts/bonus_stats.sql
+++ modeling/models/facts/bonus_stats.sql
@@ -1,22 +1,3 @@
-
 WITH h_a_players_flat_stats AS ({{ flatten_stats_from_raw_fixtures('bonus') }})
-SELECT 
-	code,
-	gameweek,
-	kickoff_time, 
-	h_player_stats.value as stat_value, 
-	h_player_stats.element as player_id,
-	'home' AS side
-FROM h_a_players_flat_stats 
-WHERE h_player_stats.value not null
-UNION ALL
-SELECT 
-	code,
-	gameweek,
-	kickoff_time, 
-	a_player_stats.value as stat_value, 
-	a_player_stats.element as player_id,
-	'away' AS side
-FROM h_a_players_flat_stats
-WHERE a_player_stats.value not null
 
+SELECT * FROM h_a_players_flat_stats
