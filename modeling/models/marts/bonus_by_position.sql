4c184d0a0ff4f0b2e480da0996ef8326b1b62a37
Omar <omarfessy@gmail.com>
2025-01-12 22:47:15 +0100

    fix bonus by position model

--- modeling/models/marts/bonus_by_position.sql
+++ modeling/models/marts/bonus_by_position.sql
@@ -6,18 +6,23 @@
 
 WITH jointure AS (
     SELECT 
-        bronze_players.player_name,
-        bronze_players.position, 
+        players.player_name,
+        players.player_position, 
         bonus_stats.gameweek_id,  
         bonus_stats.stat_value AS bonus
-    FROM {{ref('bonus_stats' )}}
-    JOIN bronze_players 
-    ON bonus_stats.player_id = bronze_players.player_id
+    FROM {{ref('bonus_stats')}} bonus_stats
+    JOIN {{ref('players_cleansed')}} players
+    ON bonus_stats.player_id = players.player_id
 )
 SELECT 
-    position, 
+    CASE 
+    WHEN player_position = 1 THEN 'Goal Keeper'
+    WHEN player_position = 2 THEN 'Defender'
+    WHEN player_position = 3 THEN 'Midfielder'
+    WHEN player_position = 4 THEN 'Forward'
+    END AS player_position, 
     bonus, 
     count(*) number_of_players
 FROM jointure
-GROUP BY bonus, position
+GROUP BY bonus, player_position
 ORDER BY number_of_players DESC
