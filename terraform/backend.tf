560f4324e27277aee71925f9793d76ea816f2d3a
Omar <omarfessy@gmail.com>
2025-01-31 18:12:13 +0100

    use local backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,17 +1,20 @@
 terraform {
-  backend "gcs" {
-    bucket = "tf-state-fpl"
-    prefix = "terraform/state"
+  # backend "gcs" {
+  #   bucket = "tf-state-fpl"
+  #   prefix = "terraform/state"
+  # }
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
 
-resource "google_storage_bucket" "tf-state-fpl" {
-  name          = "tf-state-fpl"
-  project       = local.project_id
-  location      = local.region
-  storage_class = "STANDARD"
-  force_destroy = false
-  versioning {
-    enabled = true
-  }
-}
+# resource "google_storage_bucket" "tf-state-fpl" {
+#   name          = "tf-state-fpl"
+#   project       = local.project_id
+#   location      = local.region
+#   storage_class = "STANDARD"
+#   force_destroy = false
+#   versioning {
+#     enabled = true
+#   }
+# }
